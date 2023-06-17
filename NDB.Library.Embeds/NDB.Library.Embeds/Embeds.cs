314407c1cb43c7442c0ba56ed8a1c5d923ae9e7f
NarodGaming <arron.faulkner@gmail.com>
2023-06-17 18:11:34 +0100

    Fixed footer & more
    Fixed footer support, added thumbnail support (currently not working)

--- NDB.Library.Embeds/NDB.Library.Embeds/Embeds.cs
+++ NDB.Library.Embeds/NDB.Library.Embeds/Embeds.cs
@@ -6,20 +6,22 @@ namespace NDB.Library.Embeds
 {
     public static class Embeds
     {
-        public static Embed EmbedBuilder(String content, String title = "", String picture = "")
+        public static Embed EmbedBuilder(String content, String title = "", String picture = "", String footer = "", String thumbnail = "")
         {
             EmbedBuilder builder = new();
             String? startTitle = NDB_Main._config["botname"];
             if (startTitle != null) { builder.Title = NDB_Main._config["botname"]; } else { title = "NDB"; }
             if (title != "") { builder.Title += " | " + title; }
             if (picture != "") { builder.ImageUrl= picture; }
+            if (thumbnail != "") { builder.ThumbnailUrl = thumbnail; }
+            if (footer != "") { builder.Footer.Text = footer; }
             String? colourString = NDB_Main._config["embedcolour"];
             if (colourString == null) { builder.Color = new Color(0x607D8B); } else { builder.Color = new Color(Convert.ToUInt32(colourString, 16)); }
             builder.Description = content;
             return builder.Build();
         }
 
-        public static Task<RestUserMessage> ReplyEmbedAsync(ISocketMessageChannel responseChannel, String content, String title = "", String picture = "")
+        public static Task<RestUserMessage> ReplyEmbedAsync(ISocketMessageChannel responseChannel, String content, String title = "", String picture = "", String footer = "", String thumbnail = "")
         {
             return responseChannel.SendMessageAsync("", false, EmbedBuilder(content, title, picture));
         }
